48e534da70e0a238241dc1ef5719bf803f3652f0
Om Nagarkar <om@Oms-MacBook-Pro.local>
2023-06-03 16:57:02 -0500

    initial commit, query from supabase

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://pqpslocxxejncgqqivpe.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBxcHNsb2N4eGVqbmNncXFpdnBlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU4Mjg5ODYsImV4cCI6MjAwMTQwNDk4Nn0.g33MEAsoUYwRX1ePcU5w8A-u8wsV3DuqMh_0rZFtijg')
